4d6a2b70951ba9024960c8b67742a84232bf5229
nxjoseph <nxjoseph@protonmail.com>
2022-10-02 12:03:05 +0300

    Revert "changed with original powermenu file, powermenu font icons changed"
    This reverts commit 3fe4661d0790f4d541ee80626c1d88ed2d4ade39.

--- private_dot_config/rofi/powermenu/executable_powermenu.sh
+++ private_dot_config/rofi/powermenu/executable_powermenu.sh
@@ -30,11 +30,6 @@ lock=""
 suspend=""
 logout=""
 
-# Message
-msg() {
-	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
-}
-
 # Variable passed to rofi
 options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 
@@ -54,17 +49,19 @@ case $chosen in
 		fi
         ;;
     $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
 			mpc -q pause
 			amixer set Master mute
 			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
         ;;
     $logout)
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			fi
+			bspc quit
+        fi
         ;;
 esac

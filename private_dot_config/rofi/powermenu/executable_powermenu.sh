7bc10678fd48509c9ddd54d6b35bb0d03fb38fc0
nxjoseph <nxjoseph@protonmail.com>
2022-10-02 12:07:58 +0300

    original powermenu file, font icons changed

--- private_dot_config/rofi/powermenu/executable_powermenu.sh
+++ private_dot_config/rofi/powermenu/executable_powermenu.sh
@@ -30,6 +30,11 @@ lock=""
 suspend=""
 logout=""
 
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
 # Variable passed to rofi
 options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 
@@ -49,19 +54,17 @@ case $chosen in
 		fi
         ;;
     $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
 			mpc -q pause
 			amixer set Master mute
 			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
         ;;
     $logout)
-			bspc quit
-        fi
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
         ;;
 esac
